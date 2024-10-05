2a23469475e9e4a073b0804d7f4de83bbf3cfd8a
ztzrk <ralif877@gmail.com>
2024-10-05 13:54:06 +0800

    feat: add spacing constant

---
+++ lib/constant/spacing_constant.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SpacingConstant {
+  // Vertical
+  static SizedBox verticalSpacing025 = const SizedBox(height: 2);
+  static SizedBox verticalSpacing050 = const SizedBox(height: 4);
+  static SizedBox verticalSpacing075 = const SizedBox(height: 6);
+  static SizedBox verticalSpacing100 = const SizedBox(height: 8);
+  static SizedBox verticalSpacing150 = const SizedBox(height: 12);
+  static SizedBox verticalSpacing200 = const SizedBox(height: 16);
+  static SizedBox verticalSpacing250 = const SizedBox(height: 20);
+  static SizedBox verticalSpacing300 = const SizedBox(height: 24);
+  static SizedBox verticalSpacing400 = const SizedBox(height: 32);
+  static SizedBox verticalSpacing500 = const SizedBox(height: 40);
+  static SizedBox verticalSpacing600 = const SizedBox(height: 48);
+  static SizedBox verticalSpacing800 = const SizedBox(height: 64);
+  static SizedBox verticalSpacing1000 = const SizedBox(height: 80);
+
+  // Horizontal
+  static SizedBox horizontalSpacing025 = const SizedBox(width: 2);
+  static SizedBox horizontalSpacing050 = const SizedBox(width: 4);
+  static SizedBox horizontalSpacing075 = const SizedBox(width: 6);
+  static SizedBox horizontalSpacing100 = const SizedBox(width: 8);
+  static SizedBox horizontalSpacing150 = const SizedBox(width: 12);
+  static SizedBox horizontalSpacing200 = const SizedBox(width: 16);
+  static SizedBox horizontalSpacing250 = const SizedBox(width: 20);
+  static SizedBox horizontalSpacing300 = const SizedBox(width: 24);
+  static SizedBox horizontalSpacing400 = const SizedBox(width: 32);
+  static SizedBox horizontalSpacing500 = const SizedBox(width: 40);
+  static SizedBox horizontalSpacing600 = const SizedBox(width: 48);
+  static SizedBox horizontalSpacing800 = const SizedBox(width: 64);
+  static SizedBox horizontalSpacing1000 = const SizedBox(width: 80);
+}
