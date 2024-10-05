0d71ae5a594094bd6858e2a885826b26e4495d1e
ztzrk <ralif877@gmail.com>
2024-10-05 13:53:35 +0800

    feat: add color constant

---
+++ lib/constant/color_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color primaryColor = Color(0xFF181C14);
+  static const Color secondaryColor = Color(0xFF3C3D37);
+  static const Color thirdColor = Color(0xFF697565);
+  static const Color fourthColor = Color(0xFFECDFCC);
+}
