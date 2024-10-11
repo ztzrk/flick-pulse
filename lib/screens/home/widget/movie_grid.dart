c84812f5dfd32dfebc43c621d84232e6f7448b5d
ztzrk <ralif877@gmail.com>
2024-10-11 21:58:03 +0800

    fix: shimmer card

--- lib/screens/home/widget/movie_grid.dart
+++ lib/screens/home/widget/movie_grid.dart
@@ -1,6 +1,6 @@
 import 'package:flick_pulse/controllers/movies_controller.dart';
 import 'package:flick_pulse/screens/home/widget/movie_card.dart';
-import 'package:flick_pulse/screens/home/widget/shimmer_place_holder.dart';
+import 'package:flick_pulse/screens/home/widget/shimmer_card.dart';
 import 'package:flutter/material.dart';
 
 class MovieGrid extends StatelessWidget {
@@ -25,16 +25,14 @@ class MovieGrid extends StatelessWidget {
         mainAxisSpacing: 8,
         crossAxisSpacing: 4,
       ),
-      // Calculate itemCount more safely
       itemCount: moviesController.moviesList.length +
           (moviesController.isLoading.value ? 6 : 0),
       itemBuilder: (context, index) {
-        // Check if the index is within the bounds of moviesList
         if (index < moviesController.moviesList.length) {
           final movie = moviesController.moviesList[index];
           return MovieCard(movie: movie);
         } else {
-          return const ShimmerPlaceHolder();
+          return const ShimmerCard();
         }
       },
     );
