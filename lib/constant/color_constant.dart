7783922aa925384b88d97a5379deebbc1510ff53
ztzrk <ralif877@gmail.com>
2024-10-12 07:53:22 +0800

    feat: add some color

--- lib/constant/color_constant.dart
+++ lib/constant/color_constant.dart
@@ -5,4 +5,10 @@ class ColorConstant {
   static const Color secondaryColor = Color(0xFF3C3D37);
   static const Color thirdColor = Color(0xFF697565);
   static const Color fourthColor = Color(0xFFECDFCC);
+
+  static const Color selectedBackgroundColor = Color(0xFFF2C14E);
+  static const Color selectedTextColor = Color(0xFF1C3A3E);
+
+  static const Color selectedTextNavbarColor = Color(0xFFECDFCC);
+  static const Color unselectedTextNavbarColor = Color(0xFF3C3D37);
 }
