c7e49f39734d48f0d7245f4475d51825f8947433
ztzrk <ralif877@gmail.com>
2024-10-12 07:55:46 +0800

    feat: add favorite screen

---
+++ lib/screens/favorite/favorite_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
