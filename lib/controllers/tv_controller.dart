17938d387a5e970968125f7c790e7a21ccca437e
ztzrk <ralif877@gmail.com>
2024-10-14 11:42:21 +0800

    fix: moving scroll controller and adding multi fetch category api

--- lib/controllers/tv_controller.dart
+++ lib/controllers/tv_controller.dart
@@ -1,33 +1,91 @@
 import 'package:flick_pulse/models/tv_model.dart';
 import 'package:flick_pulse/services/tv_service.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
-class TVController extends GetxController {
+enum TvCategory { popular, topRated, airingToday }
+
+class TvController extends GetxController {
   var isLoading = false.obs;
   var tvShowsList = <Tv>[].obs;
+  var selectedCategory = TvCategory.popular.obs;
   var currentPage = 1;
+  var hasMorePages = true.obs;
+  late ScrollController scrollController;
 
   @override
   void onInit() {
     super.onInit();
-    fetchTVShows();
+    scrollController = ScrollController();
+    scrollController.addListener(_scrollListener);
+    fetchTvShows();
   }
 
-  void fetchTVShows({bool isLoadMore = false}) async {
-    if (isLoading.value) return;
+  void _scrollListener() {
+    if (scrollController.position.pixels >=
+            scrollController.position.maxScrollExtent - 200 &&
+        !isLoading.value &&
+        hasMorePages.value) {
+      fetchTvShows(isLoadMore: true);
+    }
+  }
+
+  Future<void> fetchTvShows({bool isLoadMore = false}) async {
+    if (isLoading.value || (!isLoadMore && !hasMorePages.value)) return;
 
     isLoading.value = true;
 
     if (!isLoadMore) {
       currentPage = 1;
+      hasMorePages.value = true;
       tvShowsList.clear();
     } else {
       currentPage++;
     }
 
-    List<Tv> tvShows = await TvService.fetchPopularTVShows(page: currentPage);
+    List<Tv> tvShows = [];
 
-    tvShowsList.addAll(tvShows);
+    switch (selectedCategory.value) {
+      case TvCategory.popular:
+        tvShows = await TvService.fetchPopularTVShows(page: currentPage);
+        break;
+      case TvCategory.topRated:
+        tvShows = await TvService.fetchTopRatedTVShows(page: currentPage);
+        break;
+      case TvCategory.airingToday:
+        tvShows = await TvService.fetchAiringTodayTVShows(page: currentPage);
+        break;
+    }
+
+    if (tvShows.isEmpty) {
+      hasMorePages.value = false;
+    }
+
+    if (isLoadMore) {
+      tvShowsList.addAll(tvShows);
+    } else {
+      tvShowsList.value = tvShows;
+    }
+
     isLoading.value = false;
+  }
+
+  void changeCategory(TvCategory category) {
+    if (selectedCategory.value != category) {
+      selectedCategory.value = category;
+
+      if (scrollController.hasClients) {
+        scrollController.jumpTo(0);
+      }
+
+      tvShowsList.clear();
+      fetchTvShows();
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
   }
 }
