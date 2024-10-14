213822c56afefe27fa957f5232b3f5f99a2fa1c9
ztzrk <ralif877@gmail.com>
2024-10-14 11:41:07 +0800

    fix: update scrollControlle on mainController separate to tv and movies

--- lib/controllers/main_controller.dart
+++ lib/controllers/main_controller.dart
@@ -1,38 +1,19 @@
-import 'package:flick_pulse/controllers/movies_controller.dart';
 import 'package:get/get.dart';
-import 'package:flutter/material.dart';
+import 'movies_controller.dart';
+import 'tv_controller.dart';
 
 class MainController extends GetxController {
   var selectedIndex = 0.obs;
-  final scrollController = ScrollController();
-
-  @override
-  void onInit() {
-    super.onInit();
-    scrollController.addListener(_onScroll);
-  }
 
   void changeTab(int index) {
     selectedIndex.value = index;
-  }
 
-  void scrollToTop() {
-    if (scrollController.hasClients) {
-      scrollController.jumpTo(0);
+    if (index == 0) {
+      final MoviesController moviesController = Get.find<MoviesController>();
+      moviesController.scrollController.jumpTo(0);
+    } else if (index == 1) {
+      final TvController tvController = Get.find<TvController>();
+      tvController.scrollController.jumpTo(0);
     }
-  }
-
-  void _onScroll() {
-    final moviesController = Get.find<MoviesController>();
-    if (scrollController.position.pixels >=
-        scrollController.position.maxScrollExtent - 200) {
-      moviesController.fetchMovies(isLoadMore: true);
-    }
-  }
-
-  @override
-  void dispose() {
-    scrollController.dispose();
-    super.dispose();
   }
 }
