7378b272762df90721d8a714f6010c358a0290ca
ztzrk <ralif877@gmail.com>
2024-10-14 11:45:40 +0800

    fix: make the tv service can handle many category

--- lib/services/tv_service.dart
+++ lib/services/tv_service.dart
@@ -3,23 +3,48 @@ import 'package:flick_pulse/env.dart';
 import 'package:flick_pulse/models/tv_model.dart';
 
 class TvService {
-  static final Dio _dio = Dio();
-  static const String _baseUrl = 'https://api.themoviedb.org/3/tv';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static String apiKey = Env.apiKey;
 
   static Future<List<Tv>> fetchPopularTVShows({int page = 1}) async {
     try {
-      final response = await _dio.get('$_baseUrl/popular', queryParameters: {
-        'api_key': Env.apiKey,
+      var response = await Dio().get('$baseUrl/tv/popular', queryParameters: {
+        'api_key': apiKey,
         'page': page,
       });
 
-      List<Tv> tvShows = (response.data['results'] as List)
-          .map((tv) => Tv.fromJson(tv))
-          .toList();
-
-      return tvShows;
+      List results = response.data['results'];
+      return results.map((tv) => Tv.fromJson(tv)).toList();
     } catch (e) {
-      print(e);
+      return [];
+    }
+  }
+
+  static Future<List<Tv>> fetchTopRatedTVShows({int page = 1}) async {
+    try {
+      var response = await Dio().get('$baseUrl/tv/top_rated', queryParameters: {
+        'api_key': apiKey,
+        'page': page,
+      });
+
+      List results = response.data['results'];
+      return results.map((tv) => Tv.fromJson(tv)).toList();
+    } catch (e) {
+      return [];
+    }
+  }
+
+  static Future<List<Tv>> fetchAiringTodayTVShows({int page = 1}) async {
+    try {
+      var response =
+          await Dio().get('$baseUrl/tv/airing_today', queryParameters: {
+        'api_key': apiKey,
+        'page': page,
+      });
+
+      List results = response.data['results'];
+      return results.map((tv) => Tv.fromJson(tv)).toList();
+    } catch (e) {
       return [];
     }
   }
