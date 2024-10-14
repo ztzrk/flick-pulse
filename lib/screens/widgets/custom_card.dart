8a07319f3992989616f30cbc1871da930e474882
ztzrk <ralif877@gmail.com>
2024-10-14 11:44:34 +0800

    fix: update card so will be generic

--- lib/screens/widgets/custom_card.dart
+++ lib/screens/widgets/custom_card.dart
@@ -1,11 +1,16 @@
-import 'package:flick_pulse/constant/color_constant.dart';
-import 'package:flick_pulse/models/movie_model.dart';
-import 'package:flick_pulse/screens/widgets/shimmer_place_holder.dart';
 import 'package:flutter/material.dart';
+import 'package:flick_pulse/constant/color_constant.dart';
+import 'package:flick_pulse/screens/widgets/shimmer_place_holder.dart';
 
-class CustomCard extends StatelessWidget {
-  final Movie movie;
-  const CustomCard({super.key, required this.movie});
+class CustomCard<T> extends StatelessWidget {
+  final T item;
+  final String Function(T) getPosterPath;
+
+  const CustomCard({
+    super.key,
+    required this.item,
+    required this.getPosterPath,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +18,7 @@ class CustomCard extends StatelessWidget {
       color: ColorConstant.secondaryColor,
       clipBehavior: Clip.antiAlias,
       child: Image.network(
-        'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+        'https://image.tmdb.org/t/p/w500${getPosterPath(item)}',
         fit: BoxFit.cover,
         width: double.infinity,
         loadingBuilder: (context, child, loadingProgress) {
